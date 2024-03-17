81250589746587769325bdd1a8e3ab039c682fc9
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-03-17 21:15:03 +0200

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv) - 1
+    if argc < 1:
+        print("{} argunments.".format(argc))
+    elif argc == 1:
+        print("{} argunment".format(argc))
+    else:
+        print("{} argunments.".format(argc))
+    for counter in range(1, argc + 1):
+        print("{}: {:s}".format(counter, argv[counter]))
