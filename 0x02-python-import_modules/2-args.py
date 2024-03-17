259d4160a13cfa5540c3d0445bd7d9eed648775f
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-03-17 21:17:47 +0200

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     from sys import argv
     argc = len(argv) - 1
     if argc < 1:
-        print("{} argunments.".format(argc))
+        print("{} arguments.".format(argc))
     elif argc == 1:
-        print("{} argunment:".format(argc))
+        print("{} argument:".format(argc))
     else:
-        print("{} argunments:".format(argc))
+        print("{} arguments:".format(argc))
     for counter in range(1, argc + 1):
         print("{}: {:s}".format(counter, argv[counter]))
