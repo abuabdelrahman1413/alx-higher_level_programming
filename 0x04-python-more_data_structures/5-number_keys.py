e59044f81959cdf000ecd746c286d8e6901a1650
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-03-20 23:32:09 +0200

    5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    list_of_keys = a_dictionary.keys()
+    count_of_keys = 0
+    for i in list_of_keys:
+        count_of_keys += 1
+    return count_of_keys
