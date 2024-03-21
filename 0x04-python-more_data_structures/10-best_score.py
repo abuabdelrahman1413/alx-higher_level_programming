26a7289c0f3e079482804c0a9c719ded74132b90
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-03-21 03:50:25 +0200

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    values = list(a_dictionary.values())
-    max = values[0]
-    for i in values:
-        if max < i:
-            max = i
-    idx = values.index(max)
-    keys = list(a_dictionary.keys())
-    max_key = keys[idx]
-    return max_key
+    if a_dictionary:
+        values = list(a_dictionary.values())
+        max = values[0]
+        for i in values:
+            if max < i:
+                max = i
+        idx = values.index(max)
+        keys = list(a_dictionary.keys())
+        max_key = keys[idx]
+        return max_key
+    else:
+        return None
