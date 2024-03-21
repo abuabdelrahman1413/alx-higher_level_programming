842371d87b81f90ba8a0e71d82ca3c7b78e353dc
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-03-21 02:13:55 +0200

    7.1

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,24 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    sorted_dict = dict(sorted(a_dictionary.items()))
-    for key, value in sorted_dict.items():
-        print("{}: {}".format(key, value))
 def update_dictionary(a_dictionary, key, value):
-#    b_dic = a_dictionary.copy()
-   a_dictionary[key] = value
-   return a_dictionary
-
-
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
-
-print("--")
-print("--")
-
-new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
+    a_dictionary[key] = value
+    return a_dictionary
