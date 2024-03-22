18f42a0cbc06f0c858938d0c4070cda44dbc5378
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-03-22 10:31:29 +0200

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python3
-def romanToInt(self, s):
-    roman = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000,
-        "IV": 4,
-        "IX": 9,
-        "XL": 40,
-        "XC": 90,
-        "CD": 400,
-        "CM": 900,
-    }
-    i = 0
-    num = 0
-    while i < len(s):
-        if i + 1 < len(s) and s[i: i + 2] in roman:
-            num += roman[s[i: i + 2]]
-            i += 2
-        else:
-            num += roman[s[i]]
-            i += 1
-    return num
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
+    else:
+        roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
+        res = 0
+        for i in range(len(roman_string)):
+            if i + 1 < len(roman_string) and roman[roman_string[i]] < roman[roman_string[i  + 1]]:
+                res -= roman[roman_string[i]]
+            else:
+                res += roman[roman_string[i]]
+    return res
+
