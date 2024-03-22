55ef7f9bdf9c0d9ae5d61938e6c2ad44977b4dea
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-03-22 14:48:34 +0200

    100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    itr = 0
+    counter = len(my_list)
+    for i in range(counter):
+        itr = itr + my_list[i][1]
+    sum = 0
+    for i in range(counter):
+        sum += my_list[i][0] * my_list[i][1]
+    avg = sum / itr
+    return avg
