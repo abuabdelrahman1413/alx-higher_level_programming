da552f0cd45cfb680a3b675887886c8a464bac9e
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-04-22 08:48:32 +0200

    update

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for Base class"""
+
+class Base:
+    """A representation of the base of our oop hierarchy."""
+
+    __nb_objects = 0
+
+    def __init__ (self, id=None):
+        """constuctor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
