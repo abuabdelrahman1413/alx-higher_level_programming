724e2db16d7c41046e38a840b0895d22a12e62be
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 17:58:57 +0200

    6

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,6 +1,5 @@
 -- This SQL statement retrieves information about the columns in a specific table.
 -- It provides details such as column name, data type, and any constraints applied to the column.
 
-SELECT *
-  FROM ;
+SELECT *;
 ;  -- Executes the command to display column information for the table named 'table_name'.
