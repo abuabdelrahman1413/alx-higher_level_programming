c6ac24b0cb5bf33fb480103ffa32ef71c2d6aa75
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 19:19:39 +0200

    13

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- This SQL statement deletes rows from the 'second_table' where the score is less than or equal to 5.
+DELETE FROM second_table  -- Deletes rows from the 'second_table'
+WHERE score <= 5;         -- The condition for deletion: score must be less than or equal to 5
