674e2b342350d20991fa267273d2f577ffcdad57
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 18:17:26 +0200

    8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,7 +1,6 @@
-/*
-This SQL query counts the number of rows in the 'first_table' table 
-where the 'id' column is equal to 89. 
-*/
+--This SQL query counts the number of rows in the 'first_table' table 
+--where the 'id' column is equal to 89. 
+
 
 SELECT COUNT(id)  -- Counts the number of rows where the 'id' column is 89
 FROM first_table   -- Specifies the table to query ('first_table')
