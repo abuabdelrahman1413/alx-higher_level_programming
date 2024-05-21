cfe847d53222efb6173475dbad93f8210eec38ff
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 16:58:21 +0200

    2

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- This SQL statement is used to delete an entire database.
+-- Be very careful with this command as it will permanently remove the database and all its data.
+DROP DATABASE hbtn_0c_0; -- This line specifies the database to be deleted, in this case, it's 'hbtn_0c_0'.
