c8bd3327b9fa7f557a106101cd0f1d6a319dbb01
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 18:12:34 +0200

    7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- This SQL statement inserts a new row into the 'first_table' table.
+
+INSERT INTO first_table (id, name)  -- Specifies the table ('first_table') and the columns ('id', 'name') to insert data into.
+VALUES (89, 'Best School');          -- Provides the values to be inserted into the specified columns. 
+                                      -- In this case, 89 is inserted into the 'id' column and 'Best School' into the 'name' column.
