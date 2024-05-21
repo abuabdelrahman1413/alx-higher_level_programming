70f753c025f79d3f4ebc67911e1629a2eaaaa434
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 19:32:40 +0200

    15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- This SQL query analyzes the distribution of scores in the 'second_table' by grouping scores and counting their occurrences.
-SELECT score, COUNT(score) number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) number FROM second_table GROUP BY score ORDER BY number DESC;
