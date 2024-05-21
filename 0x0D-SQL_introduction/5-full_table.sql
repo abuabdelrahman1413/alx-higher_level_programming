0961bf68e0bf9d8fa5e1624f94118bedc65afa94
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 18:10:16 +0200

    5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
 -- This SQL statement retrieves information about the columns in a specific table.
 -- It provides details such as column name, data type, and any constraints applied to the column.
-
-SHOW COLUMNS FROM first_table;  -- Executes the command to display column information for the table named 'table_name'.
+SHOW COLUMNS FROM first_table;
