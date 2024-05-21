fde468c3276f8942cfb9eddfd0aee06f3a2dcf54
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 18:18:22 +0200

    8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -2,6 +2,6 @@
 --where the 'id' column is equal to 89. 
 
 
-SELECT COUNT(id)  -- Counts the number of rows where the 'id' column is 89
+SELECT COUNT(*)  -- Counts the number of rows where the 'id' column is 89
 FROM first_table   -- Specifies the table to query ('first_table')
 WHERE id = 89;     -- Filters the rows to only include those where 'id' is 89
