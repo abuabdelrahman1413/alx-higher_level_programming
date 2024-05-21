e1f6eab07c5df9f8ff72c24842f81a926ac8a0cb
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 17:56:01 +0200

    6

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -2,5 +2,5 @@
 -- It provides details such as column name, data type, and any constraints applied to the column.
 
 SELECT *
-  FROM customers;
+  FROM hbtn_0c_0;
 ;  -- Executes the command to display column information for the table named 'table_name'.
