1601898535ce0a702fb65aecdb5497657d50c8a6
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 17:04:26 +0200

    3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,5 @@
--- Assuming the database name is passed as an argument called 'database_name'
-SHOW TABLES FROM `database_name`;
+-- code to show all table of database where database passed as argument
+-- This SQL statement lists all tables available in the specified database.
+-- The database name is passed as an argument to the script.
+
+SHOW TABLES FROM `$1`;
