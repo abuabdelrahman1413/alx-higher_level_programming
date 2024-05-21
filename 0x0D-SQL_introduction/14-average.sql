2a827eed036076f788ea122ca205765d39765f58
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 19:22:10 +0200

    14

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- Calculate the average score of all entries in the 'second_table'.
-SELECT AVG(score) FROM second_table; -- The AVG() function calculates the average of the 'score' column.
+SELECT AVG(score) average FROM second_table; -- The AVG() function calculates the average of the 'score' column.
