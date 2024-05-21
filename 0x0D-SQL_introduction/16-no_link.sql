db2f5e7d87c899b3fe988fb18db3cadf63ab8d58
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 19:38:31 +0200

    16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
 -- This query retrieves scores and names from 'second_table', ordered by score (highest first).
 SELECT score, name -- Selects the score and name columns
 FROM second_table  -- From the 'second_table'
+WHERE name IS NOT NULL AND name != ''
 ORDER BY score DESC; -- Orders results by score in descending order
