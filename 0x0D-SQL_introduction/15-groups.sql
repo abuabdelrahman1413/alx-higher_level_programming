f80a064a02f38b22c6d300cd0de724585dd3c829
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 19:31:41 +0200

    15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,1 +1,2 @@
-SELECT score, COUNT(score) number FROM second_table GROUP BY score;    
+-- This SQL query analyzes the distribution of scores in the 'second_table' by grouping scores and counting their occurrences.
+SELECT score, COUNT(score) number FROM second_table GROUP BY score;
