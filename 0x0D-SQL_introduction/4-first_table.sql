8e07da70af1dd4822b9564770e4498b9b4a52306
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-21 17:20:53 +0200

    4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,6 @@
 -- This SQL statement creates a table named 'first_table' if it doesn't already exist.
 -- The table will have two columns: 'id' and 'name'.
-
 CREATE TABLE first_table IF NOT EXISTS ( 
-    id INT,                  -- Creates an integer column named 'id'
-    name VARCHAR(256)       -- Creates a string column named 'name' with a maximum length of 256 characters
+    id INT,
+    name VARCHAR(256)
 );
