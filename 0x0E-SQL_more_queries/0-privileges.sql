f9d7edf82d4741ad82a5b618a10677b5c1d32ca0
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-22 19:23:38 +0200

    00

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,8 +1,3 @@
--- Define user variables for flexibility
-SET @user1 = 'user_0d_1';
-SET @user2 = 'user_0d_2';
--- Display the grants for the first user
-SHOW GRANTS FOR @user1 @localhost;
-
--- Display the grants for the second user
-SHOW GRANTS FOR @user2 @localhost;
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
