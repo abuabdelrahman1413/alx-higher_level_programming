a4875870781dbeddfdbe68c3c27e8485b8c2e5d1
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-22 19:33:38 +0200

    1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,3 @@
 --creates the MySQL server user user_0d_1
--- Set a variable with the hashed password
-SET @user_pwd = PASSWORD('user_0d_1_pwd');
-
 -- Create the user with the hashed password
-CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY PASSWORD @user_pwd;
+CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY PASSWORD "user_0d_1_pwd";
