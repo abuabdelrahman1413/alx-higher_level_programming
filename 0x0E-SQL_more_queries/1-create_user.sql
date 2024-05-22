e446cdbcb1c20b54da89111eebb623f1d3868295
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-22 19:45:47 +0200

    12

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- Creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY 'user_0d_1_pwd';
 -- Grants all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost` WITH GRANT OPTION;
+GRANT ALL  ON *.* TO `user_0d_1`@`localhost`;
 
 -- Apply the changes
 FLUSH PRIVILEGES;
