334a6ce40cabc7e30cb0c1c56b567e2eb7d4e0d0
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-23 03:42:21 +0200

    8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,14 @@
+-- Select the database to work with
+USE hbtn_0d_usa;
+
+-- Select the 'name' column from the 'cities' table
+SELECT name 
+-- Filter the cities based on their 'state_id'
+FROM cities 
+-- Where the 'state_id' matches the 'id' of the state named 'California'
+WHERE state_id = (
+    -- Subquery to find the 'id' of the state named 'California'
+    SELECT id FROM states WHERE name = 'California'
+)
+-- Order the results in ascending order based on the 'id' column
+ORDER BY id ASC;
