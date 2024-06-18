500d87872b2c1a826c9150cbffcbaf528a03e767
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-06-18 11:56:02 +0200

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 // 11-second_biggest.js
 const args = process.argv.slice(2);
 const numbers = [];
-  for (let i = 0; i < args.length; i++) {
-    numbers.push(args[i]);
-  }
+for (let i = 0; i < args.length; i++) {
+  numbers.push(args[i]);
+}
 if (numbers.length < 2) {
   console.log(0);
 } else {
