822df7f74dc19b8cdd2b09dbd353743374f98039
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-06-18 11:55:05 +0200

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 // 11-second_biggest.js
 const args = process.argv.slice(2);
-const arr = args.map(Number);
-if (arr.length < 2) {
+const numbers = [];
+  for (let i = 0; i < args.length; i++) {
+    numbers.push(args[i]);
+  }
+if (numbers.length < 2) {
   console.log(0);
 } else {
-  arr.sort();
-  console.log(arr[arr.length - 1]);
+  numbers.sort();
+  console.log(numbers[numbers.length - 1]);
 }
