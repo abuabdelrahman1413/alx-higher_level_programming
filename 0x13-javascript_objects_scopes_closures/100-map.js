f48cfda036a474e7b912d08b14419e9722743889
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-06-19 21:46:14 +0200

    100

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// 100-map.js
+const { list } = require('./100-data'); // Import the list array from 100-data.js
+
+// Using map to compute a new array where each element is the original element multiplied by its index
+const newList = list.map((value, index) => value * index);
+
+// Print the original list
+console.log(list);
+
+// Print the new computed list
+console.log(newList);
