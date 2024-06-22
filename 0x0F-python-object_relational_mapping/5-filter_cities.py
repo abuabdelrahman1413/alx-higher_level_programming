970e79f8367d0bf8507eda4535a01b62aae86620
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-06-22 08:49:29 +0200

    Fix pycodestyle

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""  lists all cities from the database hbtn_0e_0_usa where the state name matches the provided argument """
+"""  lists all cities from the database hbtn_0e_0_usa where the state\
+    name matches the provided argument """
 import sys
 import MySQLdb
 
@@ -8,21 +9,21 @@ if __name__ == "__main__":
     # Connect to the MySQL database
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    
+
     # Create a cursor object
     cur = db.cursor()
-    
+
     # Execute the query
     cur.execute("""SELECT cities.name FROM cities INNER JOIN\
                 states ON cities.state_id = states.id\
                 WHERE states.name = %s""", (sys.argv[4],))
-    
+
     # Fetch all the results
     rows = cur.fetchall()
-    
+
     # Print the results
     print(", ".join([row[0] for row in rows]))
-    
+
     # Close the cursor and connection
     cur.close()
     db.close()
