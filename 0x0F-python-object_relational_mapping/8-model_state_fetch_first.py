f7d2e4555e08ac2b6d0d4495549d071d4a96d0d3
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-06-22 18:30:48 +0200

    Fix style in task 8
    i use black formatter vscode

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+""" print the first State object from the database hbtn_0e_6_usa"""
 
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    instance = session.query(State).first()
-    if instance is None:
-        print("Nothing")
+    # connect to the database
+    eng = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+
+    # create all tables
+    Base.metadata.create_all(eng)
+
+    # create session
+    session = sessionmaker(bind=eng)
+    session = session()
+
+    # Query the first state
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
     else:
-        print(instance.id, instance.name, sep=": ")
+        print("Nothing")
+    session.close()
