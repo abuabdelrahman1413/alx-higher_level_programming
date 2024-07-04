4322db1a46250b97b0a6ad13ae84ed0052646718
abuabdelrahman1413 <ms22abuabdelrahman1413>
2024-07-04 21:19:27 +0200

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #  script that takes in a URL, sends a GET request to the URL, and displays the body of the response Display only body of a 200 status code response.
-curl -s -I "$1"
+http_response=$(curl -s -o response.txt -w "%{response_code}" $"1")
+g
