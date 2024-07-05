2b56fbcce9e154c9f43764925ceb716fe52de2db
abuabdelrahman1413 <ms22abuabdelrahman1413>
2024-07-05 22:30:42 +0200

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
-Python script that fetches https://alx-intranet.hbtn.io/status
-"""
 
-import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
-        body = r.read()
+if __name__ == '__main__':
+    import urllib.request
 
-    print('Body response:')
-    print('\t- type: {}'.format(type(body)))
-    print('\t- content: {}'.format(body))
-    print('\t- utf8 content: {}'.format(body.decode('utf-8')))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
