88dd0218520392b7c8e3b4ff74dff0b4664a44cc
abuabdelrahman1413 <ms22abuabdelrahman1413>
2024-07-07 18:42:50 +0200

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,25 +4,29 @@
 
 def find_peak(list_of_integers):
     """returns peak of unsorted array many peeks"""
-    if not list_of_integers:
-        return None
-    if len(list_of_integers) == 1:
-        return 0
-    if list_of_integers[0] > list_of_integers[1]:
-        return 0
-    if list_of_integers[-1] > list_of_integers[-2]:
-        return len(list_of_integers) - 1
-    low = 1
-    high = len(list_of_integers) - 2
+    # if list is empty, return None
 
-    while low <= high:
-        mid = (low + high) // 2
-        if (
-            list_of_integers[mid] > list_of_integers[mid - 1]
-            and list_of_integers[mid] > list_of_integers[mid + 1]
-        ):
-            return mid
-        if list_of_integers[mid - 1] > list_of_integers[mid]:
-            high = mid - 1
-        else:
-            low = mid + 1
+    if not list_of_integers or len(list_of_integers) == 0:
+        return None
+
+    # if list is only one element, return that element
+    # if list has two elements, return the greater one
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    elif len(list_of_integers) == 2:
+        return max(list_of_integers[0], list_of_integers[1])
+    # if first and last elements are greater, return that
+    if list_of_integers[0] > list_of_integers[1]:
+        return list_of_integers[0]
+    elif list_of_integers[-1] > list_of_integers[-2]:
+        return list_of_integers[-1]
+    else:
+        i = 1
+        while i < len(list_of_integers) - 1:
+            if (
+                list_of_integers[i] > list_of_integers[i - 1]
+                and list_of_integers[i + 1] > list_of_integers[i]
+            ):
+                return list_of_integers[i]
+            else:
+                i += 1
