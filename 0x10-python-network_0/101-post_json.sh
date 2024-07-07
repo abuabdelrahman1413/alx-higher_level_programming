61fa5b946204ef901bca14617254cde74dba783c
abuabdelrahman1413 <ms22abuabdelrahman1413>
2024-07-07 20:06:58 +0200

    fix

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script send json to url
-curl -sX POST -H "Content-Type: application/json" -d @"$("$2")" "$1"
+curl -sX POST -H "Content-Type: application/json" -d @"$("basename $2")" "$1"
